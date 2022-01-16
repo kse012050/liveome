17ed8ec2d182e414cbceae78b08f5d289c1415f9
김성은 <kse012050@naver.com>
2022-01-17 01:20:48 +0900

    fullpage mobile js fin

--- js/main.js
+++ js/main.js
@@ -13,9 +13,42 @@ function fullPageEvent(){
         fullIdx = fullIdxCur(fullSelector);
         menuDecision(fullIdx)
     },100)
+
+    let touchStartX = 0;
+    let touchStartY = 0;
+    let touchEndX = 0;
+    let touchEndY = 0;
+    let touchX = 0;
+    let touchY = 0;
+
+    fullSelector.on('touchstart',function(e){
+        console.log(e.changedTouches[0].clientY);
+        touchStartX = e.changedTouches[0].clientX;
+        touchStartY = e.changedTouches[0].clientY;
+    });
     
-    fullSelector.on('mousewheel',function(e){
-        let wheelDelta = e.originalEvent.wheelDelta;
+    fullSelector.on('mousewheel touchend',function(e){
+        // console.log(e.type);
+        let wheelDelta = 0;
+        if(e.type == 'mousewheel'){
+            wheelDelta = e.originalEvent.wheelDelta;
+            // console.log(wheelDelta);
+        }else if(e.type == 'touchend'){
+            touchEndX = e.changedTouches[0].clientX;
+            touchEndY = e.changedTouches[0].clientY;
+
+            touchX = touchStartX - touchEndX;
+            touchY = touchStartY - touchEndY;
+
+            if(Math.abs(touchY) > Math.abs(touchX)){
+                if(touchStartY - touchEndY > 0){
+                    wheelDelta = -120;
+                }else{
+                    wheelDelta = 120;
+                }
+            }
+        }
+
         if(!$('html').is(':animated')){
             if(0 <= fullIdx && fullIdx < fullSelector.length){
                 if(wheelDelta > 0 && fullIdx != 0){
